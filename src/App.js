423ce8550911f1c8216c7a061d943dd75af7f31b
sasantha1 <sasanthasanju1111@gmail.com>
2026-01-01 00:21:18 +0530

    Update data

--- src/App.js
+++ src/App.js
@@ -180,7 +180,7 @@ function App() {
                 and staying up-to-date with industry best practices to deliver high-quality applications.
               </p>
               <p>
-                Based in Badulla, Sri Lanka, I'm always open to new opportunities, collaborations, and projects 
+                Based in Colombo, Sri Lanka, I'm always open to new opportunities, collaborations, and projects 
                 that challenge me to grow as a developer.
               </p>
               <div className="about-stats">
@@ -310,7 +310,7 @@ function App() {
                     <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
                     <circle cx="12" cy="10" r="3"></circle>
                   </svg>
-                  <span>Badulla, Sri Lanka</span>
+                  <span>Colombo, Sri Lanka</span>
                 </div>
               </div>
             </div>
